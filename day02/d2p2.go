f96e6846635ffefb810c4dea278c10132d86ee39
Paul Hadfield <hadfieldp@google.com>
2024-12-11 15:42:36 +0000

    Fix day 2 part 2.

--- day02/d2p2.go
+++ day02/d2p2.go
@@ -33,62 +33,50 @@ func makeLevel(s string) (level, error) {
 	return l, nil
 }
 
-func (l level) isSafe() bool {
-	return l.doIsSafe(1)
-}
-
-func (l level) doIsSafe(numViolsAllowed int) bool {
-	log.Printf("doIsSafe%v(viols left %d)", l, numViolsAllowed)
-	if len(l) < 2 {
-		return false
-	}
-	violIdx := -1
-	increasing, decreasing := l[0] < l[len(l)-1], l[0] > l[len(l)-1]
-	for i := 1; i < len(l); i++ {
-		change := l[i] - l[i-1]
-		if increasing {
-			if change <= 0 {
-				violIdx = i
-				break
-			}
-			if change > 3 {
-				violIdx = i
-				break
-			}
-		}
-		if decreasing {
-			if change >= 0 {
-				violIdx = i
-				break
-			}
-			if -change > 3 {
-				violIdx = i
-				break
-			}
-		}
-	}
-	if violIdx == -1 {
-		// No violation found
+func (l level) hasViolation() bool {
+	increasing, decreasing := l[0] < l[1], l[0] > l[1]
+	if !increasing && !decreasing {
 		return true
 	}
-	log.Printf("**Found violating element #%d = %d", violIdx, l[violIdx])
-	if numViolsAllowed == 0 {
-		log.Printf("Too bad, no more violations allowed")
-		return false
-	}
-
-	// Tolerate 1 violation. Try amending at each position.
-	for idx := 0; idx < len(l); idx++ {
-		log.Printf("**Try removing #%d element %d", idx, l[idx])
-		l2 := make(level, 0, len(l)-1)
-		l2 = append(l2, l[0:idx]...)
-		l2 = append(l2, l[idx+1:]...)
-		if l2.doIsSafe(numViolsAllowed - 1) {
-			log.Printf("yay, dampener has fixed it")
+	for i := 1; i < len(l); i++ {
+		change := l[i] - l[i-1]
+		if change == 0 {
+			return true
+		}
+		if change > 0 && decreasing {
+			return true
+		}
+		if change < 0 && increasing {
+			return true
+		}
+		if change > 3 || change < -3 {
 			return true
 		}
 	}
-	log.Printf("oh noes, no dampening possibility exists")
+	return false
+}
+
+func (l level) isSafe() bool {
+	log.Printf("doIsSafe%v", l)
+	if len(l) < 2 {
+		return false
+	}
+	if !l.hasViolation() {
+		log.Printf("  No violations :D\n")
+		return true
+	}
+	// Tolerate 1 violation. Try amending at each position.
+	for idx := 0; idx < len(l); idx++ {
+		log.Printf(" **Try removing #%d element %d", idx, l[idx])
+		l2 := make(level, 0, len(l)-1)
+		l2 = append(l2, l[0:idx]...)
+		l2 = append(l2, l[idx+1:]...)
+		if !l2.hasViolation() {
+			log.Printf("  yay, dampener has fixed it\n")
+			return true
+		}
+	}
+	log.Printf(" oh noes, no dampening possibility exists\n")
 	return false
 }
 
@@ -113,7 +101,7 @@ func main() {
 		if safe {
 			numSafe++
 		}
-		log.Printf("level %v: safe? %t\n", l, safe)
+		log.Printf("level %v: safe? %t\n\n\n", l, safe)
 	}
 	log.Printf("#safe levels: %d", numSafe)
 }
