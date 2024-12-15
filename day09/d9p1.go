1821a413658f1a21bf16b5cb478036b493ce1df3
Paul Hadfield <hadfieldp@google.com>
2024-12-15 16:27:44 +0000

    [wip] Debugging day 9 part 1

--- day09/d9p1.go
+++ day09/d9p1.go
@@ -20,6 +20,13 @@ type diskMapEntry struct {
 	next        *diskMapEntry
 }
 
+func (dme *diskMapEntry) String() string {
+	if dme.next != nil {
+		return fmt.Sprintf("<id:%d #blk:%d #emp:%d id_next:%d>", dme.fileID, dme.fileBlocks, dme.emptyBlocks, dme.next.fileID)
+	}
+	return fmt.Sprintf("<id:%d #blk:%d #emp:%d (last)>", dme.fileID, dme.fileBlocks, dme.emptyBlocks)
+}
+
 type diskMap struct {
 	first *diskMapEntry
 }
@@ -36,14 +43,20 @@ func (dm diskMap) String() string {
 	return s.String()
 }
 
+func tri(n int64) int64 {
+	return n * (n + 1) / 2
+}
+
 func (dm diskMap) checksum() int64 {
-	sum := int64(0)
-	pos := 0
+	sum, pos := int64(0), int64(-1)
+	prevID := -1
 	for e := dm.first; ; e = e.next {
-		for i := 0; i < e.fileBlocks; i++ {
-			sum += int64((i + pos) * e.fileID)
+		if e.fileID == prevID {
+			log.Printf("found two adjacent entries with same file ID %d!", prevID)
 		}
-		pos += e.fileBlocks
+		prevID = e.fileID
+		sum += int64(e.fileID) * (tri(pos+int64(e.fileBlocks)) - tri(pos))
+		pos += int64(e.fileBlocks)
 		if e.next == nil {
 			break
 		}
@@ -75,12 +88,16 @@ func (dm diskMap) defragOnce() bool {
 	// Find the first entry with empty blocks to fill
 	for first = dm.first; first.emptyBlocks == 0; first = first.next {
 	}
-	//log.Printf("first:%v prevLast:%v last:%v", first, prevLast, last)
+	//log.Printf("first:%v first.next:%v prevLast:%v last:%v", first, first.next, prevLast, last)
 
 	// Otherwise prepare to move a file block from the last entry to
 	// either 'first' (with empty, if the file ID matches), or to insert
 	// a block (if the file ID doesn't match).
-	if first.next.fileID != last.fileID {
+	var dest *diskMapEntry
+	if first.fileID == last.fileID && first.emptyBlocks > 0 {
+		dest = first
+	} else {
+		//log.Printf("Inserting new file entry before first.next=%v for file ID %d", first.next, last.fileID)
 		// insert new entry
 		existingNext := first.next
 		first.next = &diskMapEntry{
@@ -90,6 +107,7 @@ func (dm diskMap) defragOnce() bool {
 			next:        existingNext,
 		}
 		first.emptyBlocks = 0
+		dest = first.next
 	}
 	// When we reach the end, we need to shuffle the prevLast's empty blocks
 	// over to last's empties.
@@ -98,10 +116,14 @@ func (dm diskMap) defragOnce() bool {
 		prevLast.emptyBlocks = 0
 	}
 
-	first.next.fileBlocks++
-	first.next.emptyBlocks--
-	last.fileBlocks--
-	last.emptyBlocks++
+	// Adjust the block counts in the block we're moving the entry to, and from.
+	//log.Printf("Moving a block for file ID=%d\nfirst=%v\nfirst.next=%v\nlast=%v\n", first.next.fileID, first, first.next, last)
+	if dest != nil {
+		dest.fileBlocks++
+		dest.emptyBlocks--
+		last.fileBlocks--
+		last.emptyBlocks++
+	}
 	if last.fileBlocks == 0 {
 		prevLast.next = nil
 		prevLast.emptyBlocks += last.emptyBlocks
@@ -178,7 +200,7 @@ func main() {
 
 	//log.Printf("Before:\nfsummary: %v\ndsummary: %v", entries.fileSummary(), entries.diskSummary())
 	for i := 0; ; i++ {
-		//log.Printf("Iter %d: read\n%v\nfsummary: %v\ndsummary: %v", i, entries, entries.fileSummary(), entries.diskSummary())
+		//log.Printf("\n\nIter %d: read\n%v\nfsummary: %v\ndsummary: %v", i, entries, entries.fileSummary(), entries.diskSummary())
 		if entries.defragOnce() {
 			break
 		}
